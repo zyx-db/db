e52165d695b9dae0bfa664739688123ef9713d54
DhirajB-123 <dhirajbomma@gmail.com>
2023-11-21 22:37:18 -0800

    created bitmask with tests

--- src/utils/bitmap.rs
+++ src/utils/bitmap.rs
@@ -1,25 +1,89 @@
 pub struct Bitmap{
-    data: Vec<bool>
+    capacity: usize,
+    data: Vec<u64>
 }
 
 impl Bitmap {
     pub fn with_capacity(capacity: usize) -> Self {
         assert!(capacity > 0);
-        Bitmap { data: Vec::with_capacity(capacity) }
+        let size = {
+            let mut s = capacity / 64;
+            if capacity % 64 > 0 {
+                s += 1;
+            }
+            s
+        };
+        Bitmap { capacity, data: vec![0; size] }
     }
 
     pub fn set(&mut self, idx: usize){
-        assert!(idx < self.data.len());
-        self.data[idx] = true;
+        assert!(idx < self.capacity);
+        let offset = idx / 64;
+        let bit = idx % 64;
+        self.data[offset] |= 1 << bit;
     }
 
     pub fn unset(&mut self, idx: usize){
-        assert!(idx < self.data.len());
-        self.data[idx] = false;
+        assert!(idx < self.capacity);
+        let offset = idx / 64;
+        let bit = idx % 64;
+        let mask = 1 << bit;
+        let flipped = !mask;
+        self.data[offset] &= flipped;
     }
 
     pub fn check(&self, idx: usize) -> bool {
-        assert!(idx < self.data.len());
-        self.data[idx]
+        assert!(idx < self.capacity);
+        let offset = idx / 64;
+        let bit = idx % 64;
+        let mask = 1 << bit;
+        (self.data[offset] & mask) == mask
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Bitmap;
+
+    #[test]
+    fn set() {
+        let mut map = Bitmap::with_capacity(32);  
+        assert_eq!(map.check(0), false);
+        map.set(0);
+        assert_eq!(map.check(0), true);
+    }
+
+    #[test]
+    fn size_1() {
+        let map = Bitmap::with_capacity(64);  
+        assert_eq!(map.data.len(), 1 as usize);
+    }
+    
+    #[test]
+    fn size_big() {
+        let map = Bitmap::with_capacity(256);
+        assert_eq!(map.data.len(), 4 as usize);
+    }
+
+    #[test]
+    fn size_awk() {
+        let map = Bitmap::with_capacity(100);  
+        assert_eq!(map.data.len(), 2 as usize);
+    }
+
+    #[test]
+    fn false_positive() {
+        let mut map = Bitmap::with_capacity(3);
+        map.set(0);
+        map.set(2);
+        assert_eq!(map.check(1), false);
+    }
+
+    #[test]
+    fn unset(){
+        let mut map = Bitmap::with_capacity(1);
+        map.set(0);
+        map.unset(0);
+        assert_eq!(map.check(0), false);
     }
 }
